4df9b9d5958284b06b4734d6980054c92e56dff2
Daniel Pozzi <bonndan76@googlemail.com>
2013-09-19 21:40:20 +0200

    Sample config

--- config/config.php
+++ config/config.php
@@ -2,3 +2,5 @@
 $config = new Letterpress\Config();
 
 $config->addPaper(new \Letterpress\PaperSheet('Superpapier 2000', 700, 100, \Letterpress\PaperSheet::LONG_GRAIN));
+$config->addPaper(new \Letterpress\PaperSheet('Superpapier 3000', 700, 80, \Letterpress\PaperSheet::LONG_GRAIN));
+$config->addPaper(new \Letterpress\PaperSheet('Bonner Bogen', 600, 220, \Letterpress\PaperSheet::SHORT_GRAIN));
