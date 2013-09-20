37923f84ff6be779b65b4808726e488cccc7fbbf
Daniel Pozzi <bonndan76@googlemail.com>
2013-09-20 23:12:04 +0200

    sample prices

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 $config = new Letterpress\Config();
 
-$config->addPaper(new \Letterpress\PaperSheet('Superpapier 2000', 700, 100, \Letterpress\PaperSheet::LONG_GRAIN));
-$config->addPaper(new \Letterpress\PaperSheet('Superpapier 3000', 700, 80, \Letterpress\PaperSheet::LONG_GRAIN));
-$config->addPaper(new \Letterpress\PaperSheet('Bonner Bogen', 600, 220, \Letterpress\PaperSheet::SHORT_GRAIN));
+$config->addPaper(new \Letterpress\PaperSheet('Superpapier 2000', 700, 100, \Letterpress\PaperSheet::LONG_GRAIN, 0.5));
+$config->addPaper(new \Letterpress\PaperSheet('Superpapier 3000', 700, 80, \Letterpress\PaperSheet::LONG_GRAIN,  0.5));
+$config->addPaper(new \Letterpress\PaperSheet('Bonner Bogen', 600, 220, \Letterpress\PaperSheet::SHORT_GRAIN, 0.75));
