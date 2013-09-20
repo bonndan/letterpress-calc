87b7f3dc180f0507f6b301d04897955bdbfac0f5
Daniel Pozzi <bonndan76@googlemail.com>
2013-09-20 23:19:21 +0200

    order adding position on startup, using price per unit

--- src/Letterpress/Order.php
+++ src/Letterpress/Order.php
@@ -18,19 +18,46 @@ class Order
      */
     private $positions = array();
     
+    private $paper;
+    
+    /**
+     * Constructor.
+     * 
+     * @param \Letterpress\Config $config
+     */
     public function __construct(Config $config)
     {
         $this->config = $config;
+        $this->addPosition(1, 'Rüsten (1h)', $this->config->getPricePerHour());
+    }
+    
+    /**
+     * Puts the number of sheets on order.
+     * 
+     * @param \Letterpress\PaperSheet $paper
+     * @param int $prints
+     * @param \Letterpress\Layout $layout1
+     * @param \Letterpress\Layout $layout2
+     */
+    public function countSheets(PaperSheet $paper, $prints, Layout $layout1 = null, Layout $layout2 = null)
+    {
+        $this->paper = $paper;
+        $count1 = $layout1 ? $layout1->getNumberOfGangRuns() : 0;
+        $count2 = $layout2 ? $layout2->getNumberOfGangRuns() : 0;
+        $perSheet = max($count1, $count2);
+        
+        $requiredSheets = ceil($prints / $perSheet);
+        $this->addPosition($requiredSheets, ' Bögen bei ' . $perSheet . '/Bogen', $paper->getPrice());
     }
     
     public function setForms($count)
     {
-        $this->addPosition($count, 'Formen a €10,00', $count * 10.00);
+        $this->addPosition($count, 'Formen', 10.00);
     }
     
     public function setColors($count)
     {
-        $this->addPosition($count, $count . ' Farben: Maschine waschen a €10,00', $count * 10.00);
+        $this->addPosition($count, $count . ' Farben: Maschine waschen a €10,00', 10.00);
     }
     
     /**
