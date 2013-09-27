26c2c6dd091e45bd5004593ded4e978a57a5527c
Daniel Pozzi <bonndan76@googlemail.com>
2013-09-27 06:30:43 +0200

    removed config from git

--- config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$config = new Letterpress\Config();
-
-$config->addPaper(new \Letterpress\PaperSheet('Superpapier 2000', 700, 100, \Letterpress\PaperSheet::LONG_GRAIN, 0.5));
-$config->addPaper(new \Letterpress\PaperSheet('Superpapier 3000', 700, 80, \Letterpress\PaperSheet::LONG_GRAIN,  0.5));
-$config->addPaper(new \Letterpress\PaperSheet('Bonner Bogen', 600, 220, \Letterpress\PaperSheet::SHORT_GRAIN, 0.75));
